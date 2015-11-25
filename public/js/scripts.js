49b1d72bc83f1fb7cc88147391223981f0736736
jonatans <jonatan@jschumacher.com>
2015-11-25 12:49:06 -0500

    remove activities working

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,16 +1,33 @@
 $('.add-btn').on('click', function(){
-	console.log(this.id);
+	//console.log(this.id);
 	if(this.id === 'btn_addHotel') {
-		$('#hotelList').append(createLI('#hotelSelect'));
+		var $newLi = createLI('#hotelSelect');
+		$('#hotelList').append($newLi);
+		addEventToButton($newLi);
 	}
 	else if(this.id === 'btn_addRestaurant') {
-		$('#restaurantList').append(createLI('#restaurantSelect'));
+		var $newLi = createLI('#restaurantSelect');
+		$('#restaurantList').append($newLi);
+		addEventToButton($newLi);
 	}
 	else if(this.id === 'btn_addActivity') {
-		$('#activityList').append(createLI('#activitySelect'));
+		var $newLi = createLI('#activitySelect');
+		$('#activityList').append($newLi);
+		addEventToButton($newLi);
 	}
 });
 
 function createLI(btnId) {
 	return $('<div class="itinerary-item"><span class="title">' + $(btnId + " option:selected").text() + '</span><button class="btn btn-xs btn-danger remove btn-circle">x</button></div>');
 }
+
+function addEventToButton(element) {
+	element.on('click','.remove', function(){
+		console.dir(this);
+		this.parentNode.remove();
+	});
+}
+
+// $('#hotelList').on('click','.remove', function(){
+// 	console.dir(this.parent);
+// });
