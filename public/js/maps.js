3937c1ecef7380560f272e118c20f75b5ce4abe4
Leon Thorne <ldthorne@brandeis.edu>
2015-11-25 16:52:50 -0500

    markers working and able to add days

--- public/js/maps.js
+++ public/js/maps.js
@@ -1,84 +1,62 @@
 function initialize_gmaps() {
 
-  // initialize new google maps LatLng object
-  var myLatlng = new google.maps.LatLng(40.705189, -74.009209);
+    var styleArr = [{
+        featureType: 'landscape',
+        stylers: [{saturation: -100}, {lightness: 60}]
+    }, {
+        featureType: 'road.local',
+        stylers: [{saturation: -100}, {lightness: 40}, {visibility: 'on'}]
+    }, {
+        featureType: 'transit',
+        stylers: [{saturation: -100}, {visibility: 'simplified'}]
+    }, {
+        featureType: 'administrative.province',
+        stylers: [{visibility: 'off'}]
+    }, {
+        featureType: 'water',
+        stylers: [{visibility: 'on'}, {lightness: 30}]
+    }, {
+        featureType: 'road.highway',
+        elementType: 'geometry.fill',
+        stylers: [{color: '#ef8c25'}, {lightness: 40}]
+    }, {
+        featureType: 'road.highway',
+        elementType: 'geometry.stroke',
+        stylers: [{visibility: 'off'}]
+    }, {
+        featureType: 'poi.park',
+        elementType: 'geometry.fill',
+        stylers: [{color: '#b6c54c'}, {lightness: 40}, {saturation: -40}]
+    }];
 
-  // set the map options hash
-  var mapOptions = {
-    center: myLatlng,
-    zoom: 13,
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
-    styles: styleArr
-  };
+    // initialize new google maps LatLng object
+    var myLatlng = new google.maps.LatLng(40.705189, -74.009209);
 
-  // get the maps div's HTML obj
-  var map_canvas_obj = document.getElementById('map-canvas');
+    // set the map options hash
+    var mapOptions = {
+        center: myLatlng,
+        zoom: 13,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        styles: styleArr
+    };
 
-  // initialize a new Google Map with the options
-  var map = new google.maps.Map(map_canvas_obj, mapOptions);
+    // get the maps div's HTML obj
+    var map_canvas_obj = document.getElementById('map-canvas');
 
-  // add the marker to the map
-  var marker = new google.maps.Marker({
-    position: myLatlng,
-    title: 'Hello World!'
-  });
+    // initialize a new Google Map with the options
+    var map = new google.maps.Map(map_canvas_obj, mapOptions);
 
-  // draw some locations on the map
-  function drawLocation(location, opts) {
+    return map;
+
+}
+
+// draw some locations on the map
+function drawLocation(map, location, opts) {
     if (typeof opts !== 'object') {
-      opts = {};
+        opts = {};
     }
     opts.position = new google.maps.LatLng(location[0], location[1]);
     opts.map = map;
     var marker = new google.maps.Marker(opts);
-  }
-
-  var hotelLocation = [], restaurantLocations = [], activityLocations = [];
-
-  drawLocation(hotelLocation, {
-    icon: '/images/lodging_0star.png'
-  });
-  restaurantLocations.forEach(function(loc) {
-    drawLocation(loc, {
-      icon: '/images/restaurant.png'
-    });
-  });
-  activityLocations.forEach(function(loc) {
-    drawLocation(loc, {
-      icon: '/images/star-3.png'
-    });
-  });
+    return marker;
 }
-
-$(document).ready(function() {
-  initialize_gmaps();
-});
-
-var styleArr = [{
-  featureType: 'landscape',
-  stylers: [{ saturation: -100 }, { lightness: 60 }]
-}, {
-  featureType: 'road.local',
-  stylers: [{ saturation: -100 }, { lightness: 40 }, { visibility: 'on' }]
-}, {
-  featureType: 'transit',
-  stylers: [{ saturation: -100 }, { visibility: 'simplified' }]
-}, {
-  featureType: 'administrative.province',
-  stylers: [{ visibility: 'off' }]
-}, {
-  featureType: 'water',
-  stylers: [{ visibility: 'on' }, { lightness: 30 }]
-}, {
-  featureType: 'road.highway',
-  elementType: 'geometry.fill',
-  stylers: [{ color: '#ef8c25' }, { lightness: 40 }]
-}, {
-  featureType: 'road.highway',
-  elementType: 'geometry.stroke',
-  stylers: [{ visibility: 'off' }]
-}, {
-  featureType: 'poi.park',
-  elementType: 'geometry.fill',
-  stylers: [{ color: '#b6c54c' }, { lightness: 40 }, { saturation: -40 }]
-}];
--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,33 +1,101 @@
-$('.add-btn').on('click', function(){
-	//console.log(this.id);
-	if(this.id === 'btn_addHotel') {
-		var $newLi = createLI('#hotelSelect');
-		$('#hotelList').append($newLi);
-		addEventToButton($newLi);
-	}
-	else if(this.id === 'btn_addRestaurant') {
-		var $newLi = createLI('#restaurantSelect');
-		$('#restaurantList').append($newLi);
-		addEventToButton($newLi);
-	}
-	else if(this.id === 'btn_addActivity') {
-		var $newLi = createLI('#activitySelect');
-		$('#activityList').append($newLi);
-		addEventToButton($newLi);
-	}
-});
+var markers = [];
+var days = [];
+$(function () {
 
-function createLI(btnId) {
-	return $('<div class="itinerary-item"><span class="title">' + $(btnId + " option:selected").text() + '</span><button class="btn btn-xs btn-danger remove btn-circle">x</button></div>');
-}
+    var map = initialize_gmaps();
 
-function addEventToButton(element) {
-	element.on('click','.remove', function(){
-		console.dir(this);
-		this.parentNode.remove();
+    var createItineraryItem = function (placeName) {
+
+        var $item = $('<li></li>');
+        var $div = $('<div class="itinerary-item"></div>');
+
+        $item.append($div);
+
+        $div.append('<span class="title">'+ placeName +'</span>');
+        $div.append('<button class="btn btn-xs btn-danger remove btn-circle">x</button>');
+
+        return $item;
+
+    };
+
+    var getPlaceObject = function (typeOfPlace, nameOfPlace) {
+
+        var placeCollection = window['all_' + typeOfPlace];
+
+        return placeCollection.filter(function (place) {
+            return place.name === nameOfPlace;
+        })[0];
+
+    };
+
+    $(".add-day").on("click", function(){
+    	$(".current-day").removeClass("current-day")
+    	$(".add-day").before('<button class="btn btn-circle day-btn current-day">'+(days.length+1)+'</button>')
+    	var dayObj = {
+    		name: "day "+ (days.length+1),
+    		hotels: [],
+    		restaurants: [],
+    		activities: []
+    	};
+    	days.push(dayObj);
+    });
+
+
+    $(".day-buttons").on('click', '.day-btn', function(){
+    	$(".current-day").removeClass("current-day")
+    	$(this).addClass("current-day");
+    });
+
+
+    $('.add-place-button').on('click', function () {
+
+        var $this = $(this);
+        var sectionName = $this.parent().attr('id').split('-')[0];
+        console.log(sectionName)
+        var $listToAppendTo = $('#' + sectionName + '-list').children('ul');
+        var placeName = $this.siblings('select').val();
+        var placeObj = getPlaceObject(sectionName, placeName);
+
+        var resultMarker = drawLocation(map, placeObj.place[0].location);
+        var markerObj = {
+        	name: placeName,
+        	marker: resultMarker
+        }
+       	markers.push(markerObj)
+       	console.dir(markers)
+        // markers.push({
+        // 	name: placeName, 
+        // 	position: {
+        // 		lat: resultMarker.position.lat(), 
+        // 		lng: resultMarker.position.lat()
+        // 	}
+        // });
+        console.log(markers)
+        var $element = createItineraryItem(placeName);
+        var dayNum = $(".current-day").html()-1;
+    	days[dayNum][sectionName].push($element);
+        $listToAppendTo.append($element);
+        addEventToButton($element);
+
+
+    });
+
+
+	function addEventToButton(element) {
+		element.on('click','.remove', function(){
+			console.dir(element[0]);
+			var placeName = element[0].firstChild.innerText.slice(0, element[0].firstChild.innerText.length-1)
+			element.remove();
+			markers.indexOf()
+			for(var i=0; i<markers.length; i++){
+				if(markers[i].name === placeName){
+					markers[i].marker.setMap(null);
+					markers.splice(i,1)
+					return;
+				}
+			}
+
 	});
-}
 
-// $('#hotelList').on('click','.remove', function(){
-// 	console.dir(this.parent);
-// });
+}
+});
